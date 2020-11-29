ecf3d6b59fd84243af67f11d4bbb07d4ec7c95ad
Ababington <74577783+Ababington@users.noreply.github.com>
2020-11-29 13:57:14 -0500

    Add files via upload

---
+++ Pong/Pong/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.playerScore = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cpu = new System.Windows.Forms.PictureBox();
+            this.cpuLabel = new System.Windows.Forms.Label();
+            this.playGround = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).BeginInit();
+            this.playGround.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.timerTick);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Yellow;
+            this.player.Location = new System.Drawing.Point(12, 186);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(27, 127);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.White;
+            this.ball.Location = new System.Drawing.Point(434, 239);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(30, 30);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // playerScore
+            // 
+            this.playerScore.AutoSize = true;
+            this.playerScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerScore.ForeColor = System.Drawing.Color.Lime;
+            this.playerScore.Location = new System.Drawing.Point(105, 9);
+            this.playerScore.Name = "playerScore";
+            this.playerScore.Size = new System.Drawing.Size(38, 26);
+            this.playerScore.TabIndex = 3;
+            this.playerScore.Text = "00";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(535, 385);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(0, 0);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cpu
+            // 
+            this.cpu.BackColor = System.Drawing.Color.Fuchsia;
+            this.cpu.Location = new System.Drawing.Point(897, 230);
+            this.cpu.Name = "cpu";
+            this.cpu.Size = new System.Drawing.Size(27, 127);
+            this.cpu.TabIndex = 6;
+            this.cpu.TabStop = false;
+            // 
+            // cpuLabel
+            // 
+            this.cpuLabel.AutoSize = true;
+            this.cpuLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpuLabel.ForeColor = System.Drawing.Color.Red;
+            this.cpuLabel.Location = new System.Drawing.Point(735, 9);
+            this.cpuLabel.Name = "cpuLabel";
+            this.cpuLabel.Size = new System.Drawing.Size(38, 26);
+            this.cpuLabel.TabIndex = 7;
+            this.cpuLabel.Text = "00";
+            // 
+            // playGround
+            // 
+            this.playGround.BackColor = System.Drawing.Color.MidnightBlue;
+            this.playGround.Controls.Add(this.cpuLabel);
+            this.playGround.Controls.Add(this.cpu);
+            this.playGround.Controls.Add(this.pictureBox1);
+            this.playGround.Controls.Add(this.playerScore);
+            this.playGround.Controls.Add(this.ball);
+            this.playGround.Controls.Add(this.player);
+            this.playGround.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playGround.Location = new System.Drawing.Point(0, 0);
+            this.playGround.Name = "playGround";
+            this.playGround.Size = new System.Drawing.Size(945, 578);
+            this.playGround.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 578);
+            this.Controls.Add(this.playGround);
+            this.Name = "Form1";
+            this.Text = "Pong Game MOO ICT";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cpu)).EndInit();
+            this.playGround.ResumeLayout(false);
+            this.playGround.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Label playerScore;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox cpu;
+        private System.Windows.Forms.Label cpuLabel;
+        private System.Windows.Forms.Panel playGround;
+    }
+}
+
